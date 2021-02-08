534bf1c296cb1df9cd31e9a596d75f5854520ae1
krish7x <krishnakumar99g@gmail.com>
2021-02-08 22:19:50 +0530

    build added

---
+++ build/precache-manifest.17012ea6148ce25b7bb97bb9eb91788f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfb393df65bcb62d97e10cb285dc007",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bfa8dfae2d5aaad63a0",
+    "url": "/static/css/main.a28abae7.chunk.css"
+  },
+  {
+    "revision": "cb86f656033a07e68285",
+    "url": "/static/js/2.d96ca67c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d96ca67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bfa8dfae2d5aaad63a0",
+    "url": "/static/js/main.c4d6ae93.chunk.js"
+  },
+  {
+    "revision": "e8d23b230335e593b387",
+    "url": "/static/js/runtime-main.744fb23b.js"
+  }
+]);
